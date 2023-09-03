517fd5fcccdf0436134488341349f47e81a78e17
Ashtondup <ashtondup@gmail.com>
2023-09-03 13:03:51 +0200

    line spacing
    Added line spacing

--- Rover_Chalenge.py
+++ Rover_Chalenge.py
@@ -3,11 +3,18 @@ class Rover:
         self.x = x
         self.y = y
 
-def find_intersections(input_file):
+def find_intersections(input_file): # Argument/method that opens the file and reads the contect of the file and move the rover in the direction stated in the file.
     rovers = set()
     intersection_points = set()
     plateau_x, plateau_y = 0, 0
     terrain = []  # 2D grid to represent terrain
+    
+# context of the text file is as follow:
+# 4 4 // The upper-right coordinates of the plateau 
+# 0 2 // The first rover's initial position
+# NEESSS // The series of instructions telling the first rover how to explore the plateau
+# 4 1 // The second rover's initial position
+# WWWNNNEEE // The series of instructions telling the second rover how to explore the plateau
 
     with open(input_file, 'r') as f:
         plateau_x, plateau_y = map(int, f.readline().split())
@@ -44,11 +51,11 @@ def find_intersections(input_file):
 
     return intersection_points, terrain
 
-def print_terrain(terrain):
+def print_terrain(terrain): # Argument/method that will print the terrain
     for row in terrain:
         print(''.join(row))
 
-def main():
+def main(): # This is the main argument/method
     try:
         input_file = 'input.txt'
         intersection_points, terrain = find_intersections(input_file)
@@ -61,7 +68,7 @@ def main():
     for i, point in enumerate(intersection_points, start=1):
         print(f"Intersection point {i}: {point[0]} {point[1]}")
 
-    print("Visual representation Paths")
+    print("\nVisual representation Paths")
     print_terrain(terrain)
     print("\nLegend:")
     print("X : Start")
